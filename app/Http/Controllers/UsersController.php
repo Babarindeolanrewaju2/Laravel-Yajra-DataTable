4516a864d7c62b83e8157379c3dd57d7c0414779
ayuba <olanrewajuayuba@gmail.com>
2019-12-23 18:23:13 +0100

    wrote the code for the controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Datatables;
+use DB;
 
 class UsersController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('users');
+    }
+    public function usersList()
+    {
+        $users = DB::table('users')->select('*');
+        return datatables()->of($users)
+            ->make(true);
+    }
 }
